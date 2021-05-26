6b7993c5398d188a03651b8ff033d96ca05cd72f
Igor Melnik <igorekmelnik@gmail.com>
2021-05-27 00:27:52 +0300

    First version of SteganoTransformation

---
+++ SteganographyJPEG/DKP.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteganographyJPEG
+{
+    public static class DKP
+    {
+        public static double[,] Dkp(byte[,] one)
+        {
+            int n = one.GetLength(0);
+            double[,] two = new double[n, n];
+            double U;
+            double V;
+            double temp;
+            for (int v = 0; v < n; v++)
+            {
+                for (int u = 0; u < n; u++)
+                {
+                    if (v == 0) V = 1.0 / Math.Sqrt(2);
+                    else V = 1;
+                    if (u == 0) U = 1.0 / Math.Sqrt(2);
+                    else U = 1;
+                    temp = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            temp += one[i, j] * Math.Cos(Math.PI * v * (2 * i + 1) / (2 * n)) *
+                                Math.Cos(Math.PI * u * (2 * j + 1) / (2 * n));
+                        }
+                    }
+                    two[v, u] = U * V * temp / (Math.Sqrt(2 * n));
+                }
+            }
+            return two;
+        }
+        public static double[,] Odkp(double[,] one)
+        {
+            int n = one.GetLength(0);
+            double[,] two = new double[n, n];
+            double U;
+            double V; 
+            double temp;
+            for (int v = 0; v < n; v++)
+            {
+                for (int u = 0; u < n; u++)
+                {
+                    temp = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (i == 0) V = 1.0 / Math.Sqrt(2);
+                            else V = 1;
+                            if (j == 0) U = 1.0 / Math.Sqrt(2);
+                            else U = 1;
+                            temp += U * V * one[i, j] * Math.Cos(Math.PI * i * (2 * v + 1) / (2 * n)) *
+                                Math.Cos(Math.PI * j * (2 * u + 1) / (2 * n));
+                        }
+                    }
+                    two[v, u] = temp / (Math.Sqrt(2 * n));
+                }
+            }
+            return two;
+        }
+
+
+        public static double[,] CoefficientChange(double[,] temp, int i, int u1, int v1, int u2, int v2, int P)
+        {
+            /*
+             double - матрица коэффициентов
+             i - бит нашего сообщения (0 или 1)
+             u,v - координаты коэффициентов
+             */
+
+
+            //double[,] two = new double[one.GetLength(0), one.GetLength(1)];
+            //for (int k = 0; k < two.GetLength(0); k++)
+            //    for (int l = 0; l < two.GetLength(1); l++)
+            //        two[k, l] = one[k, l];
+            double Abs1, Abs2;
+            double z1 = 0, z2 = 0;
+            Abs1 = Math.Abs(temp[u1, v1]);
+            Abs2 = Math.Abs(temp[u2, v2]);
+            if (temp[u1, v1] >= 0) z1 = 1;
+            else z1 = -1;
+            if (temp[u2, v2] >= 0) z2 = 1;
+            else z2 = -1;
+ 
+            if (i == 0)
+            {
+                if (Abs1 - Abs2 <= P)
+                    Abs1 = P + Abs2 + 1;
+            }
+            if (i == 1)
+            {
+                if (Abs1 - Abs2 >= -P)
+                    Abs2 = P + Abs1 + 1;
+            }
+            temp[u1, v1] = z1 * Abs1;
+            temp[u2, v2] = z2 * Abs2;
+            return temp;
+        }
+    }
+}
--- SteganographyJPEG/SteganoTransformation.cs
+++ SteganographyJPEG/SteganoTransformation.cs
@@ -1,17 +1,80 @@
 using System;
+using System.Collections.Generic;
 
 namespace SteganographyJPEG
 {
     public class SteganoTransformation
     {
+        public List<double[,]> EncodeCoefficient;
+        public List<double[,]> EncodeCoefficientForWrite;
+        public List<double[,]> DecodeCoefficient;
+        public List<double[,]> DecodeCoefficientForWrite;
+        public int u1 = 0; //Координаты коэффициентов, которые будем менять
+        public int v1 = 0;
+        public int u2 = 1;
+        public int v2 = 1;
+        public int P = 25; //Порог измененеия коэффициентов
+        public string message;
+
+
         public void Encode(SteganoContainer container, SteganoMessage steganoMessage, int key)
         {
-            throw new Exception("Method in development. See the future release.");
+            //Применили дкп ко всем блокам 8х8
+            foreach(var b in container.Blocks)
+            {
+                Coefficient.Add(DKP.Dkp(b));
+            }
+
+            //Меняем коэффициенты ДКП
+            for (int i = 0; i < steganoMessage.Data.Length; i++) // Data долже быть не массивом байт, в строкой или массивом бит
+            {
+                Coefficient[i] = DKP.CoefficientChange(Coefficient[i], steganoMessage.Data[i], u1, v1, u2, v2, P);
+            }
+
+
+            foreach(var c in Coefficient)
+            {
+                CoefficientForWrite.Add(DKP.Odkp(c));
+            }
+
+
         }
 
         public void Decode(SteganoContainer container, int key)
         {
-            throw new Exception("Method in development. See the future release.");
+            //Применили ДКП к блокам 8х8
+            foreach (var block in container.Blocks)
+            {
+                DecodeCoefficient.Add(DKP.Dkp(block));
+            }
+
+            double Abs1;
+            double Abs2;
+
+            string bits = ""; //временная переменная для записи сообщения
+
+            List<byte> bytetext = new List<byte>(); //извлеченное сообщение
+            double[,] dkp_8x8; // временная переменная для одной матрицы 8на8 
+
+
+            for (int k = 0; k < /*количество сегментов 8х8*/; k++)
+            {
+                dkp_8x8 = DecodeCoefficient[k];
+                Abs1 = Math.Abs(dkp_8x8[u1, v1]);
+                Abs2 = Math.Abs(dkp_8x8[u2, v2]);
+                if (Abs1 > Abs2)
+                    bits += "0";
+                if (Abs1 < Abs2)
+                    bits += "1"; 
+                if (bits.Length == 8)
+                {
+                    bytetext.Add(Convert.ToByte(new Bits(bits).Number));
+                    bits = "";
+                }               
+            }
+
+            //В итоге список bytetext хранит наше сообщение
+            
         }
     }
 }
